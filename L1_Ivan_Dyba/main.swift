f7337337b99311e877d54716540e0ef6b01f24b4
Ivan <idyba@yandex.ru>
2021-03-11 17:30:53 +0300

    Initial Commit

---
+++ L1_Ivan_Dyba/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  L1_Ivan_Dyba
+//
+//  Created by Ivan Dyba on 10.03.2021.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//let x = "string"
+//var a = 56
+//
+//print(x)
+
+    //Array
+
+//var dreamTeam: Array = [
+//    "Igor",
+//    "Ivan",
+//    "Vasiliy"
+//]
+//
+//print(dreamTeam[0])
+//dreamTeam.append("Elena")
+//
+//dreamTeam.removeAll()
+//
+//print(dreamTeam)
+
+
+//Dictionary
+
+//var priceList: [String:Double] = [
+//
+//    "orange" : 34.67,
+//    "apple" : 45.89
+//]
+//print(priceList)
+
+//Set
+
+//var list1: Set<String> = [
+//
+//    "orange",
+//    "apple"
+//    "blue"
+//
+//]
+//var list2: Set<String> = [
+//
+//    "orange",
+//
+//]
+//let result = list2.isSubset(of: list1)
+//
+//print(result)
+
+
+
